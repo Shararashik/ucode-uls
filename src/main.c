bbf4f3e4e0070251e7fe0540a9c935f5df531405
yura-pepsicola <y.nosuch@gmail.com>
2023-12-20 16:43:51 +0200

    a

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    char *flags[argc - 1];
+    char *flags[argc - 2];
     mx_get_flags(argc, argv, flags);
     // for(int i = 0; i < argc - 2; i++) {
     //     mx_printstr(flags[i]);
