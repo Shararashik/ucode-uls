d1b777bffce2be0ed75393e648524cc5e9e8f5a6
yura-pepsicola <y.nosuch@gmail.com>
2023-12-21 14:19:31 +0200

    a

--- inc/uls.h
+++ inc/uls.h
@@ -1,15 +1,24 @@
 #include "../libmx/inc/libmx.h"
 
+#include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/acl.h>
 #include <errno.h>
 
 void mx_get_flags(int argc, char *argv[], char **flags);
 
 typedef struct flags {
-    bool R, a, A, G, h, dog, e, T, l, one, C, r, t, u, c, S;
+    bool R, a, A, G, h, dog, e, T, long, one, C, r, t, u, c, S;
 }   flags_t;
 
-
 flags_t* mx_init_flags(char **flags_str, int argc);
-
+void mx_printlong(long num);
+void mx_simple_output();
 
