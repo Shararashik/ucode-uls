a2b2dea21438a9f9a0c065542e353c137a2de043
yura-pepsicola <y.nosuch@gmail.com>
2023-12-21 18:21:29 +0200

    a

--- src/mx_simple_output.c
+++ src/mx_simple_output.c
@@ -1,20 +1,20 @@
 #include "uls.h"
 
-// void print_permissions(mode_t mode) {
-//     char permissions[10];
-//     permissions[0] = (mode & S_IRUSR) ? 'r' : '-';
-//     permissions[1] = (mode & S_IWUSR) ? 'w' : '-';
-//     permissions[2] = (mode & S_IXUSR) ? 'x' : '-';
-//     permissions[3] = (mode & S_IRGRP) ? 'r' : '-';
-//     permissions[4] = (mode & S_IWGRP) ? 'w' : '-';
-//     permissions[5] = (mode & S_IXGRP) ? 'x' : '-';
-//     permissions[6] = (mode & S_IROTH) ? 'r' : '-';
-//     permissions[7] = (mode & S_IWOTH) ? 'w' : '-';
-//     permissions[8] = (mode & S_IXOTH) ? 'x' : '-';
-//     permissions[9] = '\0';
+void print_permissions(mode_t mode) {
+    char permissions[10];
+    permissions[0] = (mode & S_IRUSR) ? 'r' : '-';
+    permissions[1] = (mode & S_IWUSR) ? 'w' : '-';
+    permissions[2] = (mode & S_IXUSR) ? 'x' : '-';
+    permissions[3] = (mode & S_IRGRP) ? 'r' : '-';
+    permissions[4] = (mode & S_IWGRP) ? 'w' : '-';
+    permissions[5] = (mode & S_IXGRP) ? 'x' : '-';
+    permissions[6] = (mode & S_IROTH) ? 'r' : '-';
+    permissions[7] = (mode & S_IWOTH) ? 'w' : '-';
+    permissions[8] = (mode & S_IXOTH) ? 'x' : '-';
+    permissions[9] = '\0';
 
-//     mx_printstr(permissions);
-// }
+    mx_printstr(permissions);
+}
 
 void mx_simple_output(void) {
     DIR *dir;
@@ -42,7 +42,7 @@ void mx_simple_output(void) {
             exit(EXIT_FAILURE);
         }
 
-        //print_permissions(file_stat.st_mode);
+        print_permissions(file_stat.st_mode);
         mx_printstr(" ");
         mx_printlong((long)file_stat.st_nlink);
         mx_printstr(" ");
