6a19fdb69dcad8e00641739d6e14a117497d9f8c
yura-pepsicola <y.nosuch@gmail.com>
2024-01-04 10:14:23 +0200

    a

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "uls.h"
 
 int main(int argc, char *argv[]) {
+    //test
     char *flags_str[argc - 1];
     mx_get_flags(argc, argv, flags_str);
     flags_t *flags = mx_init_flags(flags_str, argc - 1);
