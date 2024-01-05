1a2cd8a16162f014ca3b5ff4bf95752d6344b747
yura-pepsicola <y.nosuch@gmail.com>
2024-01-05 16:17:46 +0200

    a

--- src/mx_output_long.c
+++ src/mx_output_long.c
@@ -58,12 +58,12 @@ void mx_output_long(t_list *files, flags_t *flags) {
         pw = getpwuid(file_stat.st_uid);      
         gr = getgrgid(file_stat.st_gid);
         mx_printstr((pw != NULL) ? pw->pw_name : "unknown");
-        mx_printstr(" ");
+        mx_printstr("  ");
         mx_printstr((gr != NULL) ? gr->gr_name : "unknown");
         mx_printstr(" ");
-
+        //fix format of output down below
         mx_printlong((long long)file_stat.st_size);
-        //char *date = 
+        //char *date =    // add variations of time of modification and etc.
         time_t lastModificationTime = file_stat.st_mtime;
         mx_printstr(" ");
         char *date = ctime(&lastModificationTime);
@@ -71,11 +71,7 @@ void mx_output_long(t_list *files, flags_t *flags) {
         date += 4; 
         mx_printstr(mx_strndup(date, (size_t)(mx_strlen(date) - 9)));
         mx_printstr(" ");
-        //strftime(time_buffer, sizeof(time_buffer), "%b %d %H:%M", localtime(&file_stat.st_mtime));
-        //mx_printstr(time_buffer);
         mx_printstr(mx_get_filename(i->data));
-        // strftime(time_buffer, sizeof(time_buffer), "%b %d %H:%M", localtime(&file_stat.st_mtime));
-        // mx_printstr(time_buffer);
         // Display ACL information if available
         // acl_t acl = acl_get_file(i->data, ACL_TYPE_ACCESS);
         // if (acl != NULL) {
