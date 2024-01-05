57f073106ca977a6a74e6061bcdffdd9d6c60f63
Shararashik <shararashik@gmail.com>
2024-01-05 16:05:50 +0200

    341

--- src/mx_choose_sort.c
+++ src/mx_choose_sort.c
@@ -7,15 +7,10 @@ SortComparator mx_choose_sort(flags_t *flags) {
     }
     else if(flags->t) {
         return mx_timecmp;
-    } 
+    }  
     else {
         return mx_abccmp;
     }
-     //t) {
-    //     return mx_sort_by_mod;
-    // }
-    // else if(flags->u) {
-    //     return mx_sort_by_la;
-    // }
+
     /* else if(flags-> */
 }
