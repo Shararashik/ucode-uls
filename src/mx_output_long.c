72ece7288883dcd33f54bdd21c3210c37a285b01
yura-pepsicola <y.nosuch@gmail.com>
2024-01-08 12:40:38 +0200

    a

--- src/mx_output_long.c
+++ src/mx_output_long.c
@@ -8,6 +8,7 @@ void mx_output_long(t_list *files, flags_t *flags) {
     long link_max = 0;
     long size_max = 0;
     acl_t acl;
+    long total_blocks = 0;
     for (t_list *i = files; i; i = i->next) {
         char *permissions = mx_strdup(mx_get_permissions(file_stat.st_mode));
         ssize_t size = listxattr(mx_get_filename(i->data), NULL, 0, 0);
@@ -25,20 +26,40 @@ void mx_output_long(t_list *files, flags_t *flags) {
             mx_file_error(mx_get_filename(i->data));
             exit(EXIT_FAILURE);
         }
+        total_blocks += file_stat.st_blocks;
         if(link_max < (long)file_stat.st_nlink) {
             link_max = (long)file_stat.st_nlink;
         }
-        if(size_max < (long)file_stat.st_size) {
-            size_max = (long)file_stat.st_size;
+        if(flags->h) {
+            const char *units[] = {"B", "K", "M", "G", "T"};
+            int j = 0;
+            double size_in_units = (double)file_stat.st_size;
+            while (size_in_units >= 1024 && j < sizeof(units) / sizeof(units[0]) - 1) {
+                size_in_units /= 1024;
+                j++;
+            }
+            if(size_max < (long)mx_strlen(mx_strjoin(mx_itoa((int)size_in_units), units[j]))) {
+                size_max = (long)mx_strlen(mx_strjoin(mx_itoa((int)size_in_units), units[j]));
+            }
+        } else {
+            if(size_max < (long)file_stat.st_size) {
+                size_max = (long)file_stat.st_size;
+            }
         }
+        
     }
+    mx_printstr("total ");
+    mx_printlong(total_blocks);
+    mx_printstr("\n");
     int l_len;
     for(l_len = 0; link_max != 0; l_len++) {
         link_max /= 10;
     }
     int s_len;
-    for(s_len = 0; size_max != 0; s_len++) {
-        size_max /= 10;;
+    if(!flags->h) {
+        for(s_len = 0; size_max != 0; s_len++) {
+            size_max /= 10;;
+        }
     }
     for(t_list *i = files; i; i = i->next) {
         if (lstat(i->data, &file_stat) == -1) {
@@ -76,15 +97,30 @@ void mx_output_long(t_list *files, flags_t *flags) {
         mx_printstr("  ");
         mx_printstr((gr != NULL) ? gr->gr_name : "unknown");
         mx_printstr("  ");
-        long size_len = (long)file_stat.st_size;
-        int count;
-        for(count = 0; size_len != 0; count++) {
-            size_len /= 10;
+        //kilobytes, bytes
+        if(flags->h) {
+            const char *units[] = {"B", "K", "M", "G", "T"};
+            int j = 0;
+            double size_in_units = (double)file_stat.st_size;
+            while (size_in_units >= 1024 && j < sizeof(units) / sizeof(units[0]) - 1) {
+                size_in_units /= 1024;
+                j++;
+            }
+            for(long i = 0; i <= size_max - (long)mx_strlen(mx_strjoin(mx_itoa((int)size_in_units), units[j])); i++) {
+                mx_printstr(" ");
+            }
+            mx_printstr(mx_strjoin(mx_itoa((int)size_in_units), units[j]));
+        } else {
+            long size_len = (long)file_stat.st_size;
+            int count;
+            for(count = 0; size_len != 0; count++) {
+                size_len /= 10;
+            }
+            for(long i = 0; i < s_len - count; i++) {
+                mx_printstr(" ");
+            }
+            mx_printlong((long)file_stat.st_size); 
         }
-        for(long i = 0; i < s_len - count; i++) {
-            mx_printstr(" ");
-        }
-        mx_printlong((long)file_stat.st_size);
         time_t t_date;
         if(flags->u) {
             t_date = file_stat.st_atime;
