aac7db1afb61c5ea99056a13caeb578de22b6895
Shararashik <shararashik@gmail.com>
2024-01-10 13:28:39 +0200

    delete

--- src/main.c
+++ src/main.c
@@ -7,61 +7,7 @@ int main(int argc, char *argv[]) {
     flags_t *flags = mx_init_flags(flags_str, argc - 1);
     //code mx_get_output that returns function
     mx_uls(flags, ".", mx_get_output(flags), false);
-    // for(int i = 0; i < argc - 1; i++) {
-    //     mx_printstr(flags[i]);
-    // }
-    //if (flags->R == 1) {
-    //     mx_printstr("R worked");
-   // }
-    // if (flags->a == 1) {
-    //     mx_printstr("a worked");
-    // }    
-    // if (flags->A == 1) {
-    //     mx_printstr("A worked");
-    // }    
-    // if (flags->h == 1) {
-    //     mx_printstr("h worked");
-    // }      
-    // if (flags->G == 1) {
-    //     mx_printstr("G worked");
-    // }    
-    // if (flags->dog == 1) {
-    //     mx_printstr("@ worked");
-    // }      
-    // if (flags->e == 1) {
-    //     mx_printstr("e worked");
-    // }       
-    // if (flags->T == 1) {
-    //     mx_printstr("T worked");
-    // }   
-    //if (flags->longf == 1) {
-    //     mx_printstr("l worked");
-    //}       
-    // if (flags->one == 1) {
-    //     mx_printstr("1 worked");
-    // }     
-    // if (flags->C == 1) {
-    //     mx_printstr("C worked");
-    // }         
-    // if (flags->r == 1) {
-    //     mx_printstr("r worked");
-    // }               
-    // if (flags->t == 1) {
-    //     mx_printstr("t worked");
-    // }        
-    // if (flags->u == 1) {
-    //     mx_printstr("u worked");
-    // }     
-    // if (flags->c == 1) {
-    //     mx_printstr("c worked");
-    // }         
-    // if (flags->S == 1) {
-    //     mx_printstr("S worked");
-    // }
-    //mx_simple_output();   
-    //if (flags->one == 1) {
-    //     mx_output_with_flag_one(&flags);
-    // }
+ 
     return 0;
 }
 
