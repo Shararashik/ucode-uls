55a15954ed2b1756eccc8e55c189bb62794e28a7
yura-pepsicola <y.nosuch@gmail.com>
2024-01-10 16:31:05 +0200

    a

--- src/mx_output_long.c
+++ src/mx_output_long.c
@@ -152,13 +152,23 @@ void mx_output_long(t_list *files, flags_t *flags) {
                 free(acl_text);
                 mx_printstr("\n");
             }
-        } else if(flags->dog) {
+        if(flags->dog) {
             char *attrBuffer = (char *)malloc((size_t)size);
             ssize_t attrCount = listxattr(i->data, attrBuffer, (size_t)size, 0);
             for (ssize_t i = 0; i < attrCount; i += mx_strlen(&attrBuffer[i]) + 1) {
                 mx_printchar(attrBuffer[i]);
             }
             mx_printstr("\n");
+        }
+        if(flags->e) {
+            acl = acl_get_file(i->data, ACL_TYPE_ACCESS);
+            if(acl) {
+                char *acl_text = acl_to_text(acl, NULL);
+                mx_printstr(acl_text);
+                acl_free(acl);
+                free(acl_text);
+                mx_printstr("\n");
+            }
         }
 
     }
