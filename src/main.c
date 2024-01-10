2a73ebee5b7d1dea89c0aa5cf7fdfe8cf843b4ab
yura-pepsicola <y.nosuch@gmail.com>
2024-01-10 19:09:56 +0200

    a

--- src/main.c
+++ src/main.c
@@ -6,8 +6,23 @@ int main(int argc, char *argv[]) {
     mx_get_flags(argc, argv, flags_str);
     flags_t *flags = mx_init_flags(flags_str, argc - 1);
     //code mx_get_output that returns function
-    mx_uls(flags, ".", mx_get_output(flags), false, flags->files_and_flags);
- 
+    OutputFunction output = mx_get_output(flags);
+    if(flags->files_and_flags) {
+        if(flags->files) {
+             if (flags->r) {
+                mx_reverse_list(&flags->files);
+            }
+            SortComparator sort = mx_choose_sort(flags);
+            mx_sort_list(flags->files, sort);
+            output(flags->files, flags);
+        }
+        for(t_list *i = flags->folders ;i; i = i->next) {
+            mx_uls(flags, i->data, output, true, flags->files_and_flags);
+        }
+    } else {
+        mx_uls(flags, ".", output, false, flags->files_and_flags);
+    }
+    free(flags);
     return 0;
 }
 
--- src/mx_uls.c
+++ src/mx_uls.c
@@ -3,16 +3,7 @@
 void mx_uls(flags_t *flags, char *path, OutputFunction output, bool title, bool fnf) {
     SortComparator sort = mx_choose_sort(flags);
     t_list *files;
-    if(flags->files_and_flags && fnf) {
-        files = flags->folders;
-        if(flags->files) {
-            mx_sort_list(flags->files, sort);
-        }
-        
-    } else {
-        files = mx_get_files(path, flags);
-    }
-    
+    files = mx_get_files(path, flags);
     // for(t_list *i = flags->files ;i; i = i->next) {
     //     mx_printstr(i->data);
     //     mx_printstr("\n");
@@ -21,25 +12,12 @@ void mx_uls(flags_t *flags, char *path, OutputFunction output, bool title, bool 
     mx_sort_list(files, sort);
     if (flags->r) {
         mx_reverse_list(&files);
-        if(flags->files) {
-           mx_reverse_list(&flags->files); 
-        }
     }
-    if(flags->files && fnf) {
-        output(flags->files, flags);
-    }
-    if(flags->files_and_flags && title) {
-        mx_printstr(mx_get_filename(path));
-        mx_printstr(":\n"); 
-    }
-    else if(title) {
+    if(title) {
         mx_printstr(path);
         mx_printstr(":\n");
     }
-    if(!fnf) {
-        output(files, flags);
-    }
-    if(flags->R || fnf) {
+    if(flags->R) {
         for(; files; files = files->next) {
             struct stat entry;
             if(!lstat(files->data, &entry)) {
