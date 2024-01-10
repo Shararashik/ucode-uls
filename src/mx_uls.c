fc6716a8cd5046df8bf7254ebcd1e89b5e896c95
yura-pepsicola <y.nosuch@gmail.com>
2024-01-10 18:51:51 +0200

    a

--- src/mx_uls.c
+++ src/mx_uls.c
@@ -5,13 +5,14 @@ void mx_uls(flags_t *flags, char *path, OutputFunction output, bool title, bool 
     t_list *files;
     if(flags->files_and_flags && fnf) {
         files = flags->folders;
-        mx_sort_list(flags->files, sort);
+        if(flags->files) {
+            mx_sort_list(flags->files, sort);
+        }
+        
     } else {
         files = mx_get_files(path, flags);
     }
-    if(flags->files && fnf) {
-        output(flags->files, flags);
-    }
+    
     // for(t_list *i = flags->files ;i; i = i->next) {
     //     mx_printstr(i->data);
     //     mx_printstr("\n");
@@ -20,7 +21,12 @@ void mx_uls(flags_t *flags, char *path, OutputFunction output, bool title, bool 
     mx_sort_list(files, sort);
     if (flags->r) {
         mx_reverse_list(&files);
-        mx_reverse_list(&flags->files);
+        if(flags->files) {
+           mx_reverse_list(&flags->files); 
+        }
+    }
+    if(flags->files && fnf) {
+        output(flags->files, flags);
     }
     if(flags->files_and_flags && title) {
         mx_printstr(mx_get_filename(path));
